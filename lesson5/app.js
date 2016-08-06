639cbb5da11b53a48d3e56830723c99e8544c406
hentaicracker <641232582@qq.com>
2016-08-06 18:07:37 +0800

    lesson 5

--- lesson5/app.js
+++ lesson5/app.js
@@ -1,14 +1,24 @@
-var express = require('express');
-var utility = require('utility');
+var async = require('async');
 
-var app = express();
+var count = 0;
+var fetchUrl = function (url, callback) {
+    var delay = parseInt((Math.random() * 10000000) % 2000, 10);
+    count++;
+    console.log('现在的并发数是' + count + '，正在抓取的是' + url + '，耗时' + delay + '毫秒');
+    setTimeout(function () {
+        count--;
+        callback(null, url + ' html content');
+    }, delay);
+};
 
-app.get('/', function(req, res){
-    var q = req.query.q;
-    var md5Value = utility.md5(q);
-    res.send(md5Value);
-});
+var urls = [];
+for (var i = 0; i < 30; i++) {
+    urls.push('http://datasource_' + i);
+}
 
-app.listen(3003, function(){
-    console.log('app is listening at port 3003');
+async.mapLimit(urls, 5, function (url, callback) {
+    fetchUrl(url, callback);
+}, function (err, result) {
+    console.log('final:');
+    console.log(result);
 });
